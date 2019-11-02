723558a11d4d278eaac720e1574ee75e55ed9370
Ossama <poohd1ni@yahoo.com>
2019-11-02 16:17:12 -0400

    first commit

---
+++ amer_p1/src/amer_p1/Application.java
@@ -0,0 +1,14 @@
+package amer_p1;
+
+
+public class Application {
+
+	   public static void main(String[] args) {
+	      
+	       DuplicateRemover dp = new DuplicateRemover();
+	      
+	       dp.remove("problem1.txt");
+	       dp.write("unique_words.txt");
+	      
+	   }
+	}
---
+++ amer_p2/src/amer_p2/Application.java
@@ -0,0 +1,12 @@
+package amer_p2;
+
+
+public class Application {
+	   public static void main(String[] args) {
+	       String inFilename = "problem2.txt";
+	       String outFilename = "unique_word_counts.txt";
+	       DuplicateCounter dc = new DuplicateCounter();
+	       dc.count(inFilename);
+	       dc.write(outFilename);
+	   }
+	}
